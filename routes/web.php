ebbd52b3dcfcf609a222aeda6869f2997acfc7c5
agungrestuhaikal <agungrestuhaikal@gmail.com>
2026-05-09 19:53:49 +0700

    SCRUM 13 Manajemen Profil & Portofolio Kontribusi (MS) & SCRUM 15 Manajemen Statistik (MS)

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\LaporanController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\StatistikController;
 
 Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
 Route::post('/login', [AuthController::class, 'login'])->name('login.post');
@@ -21,10 +23,14 @@ Route::middleware(['auth'])->group(function () {
 
     Route::middleware('role:masyarakat')->prefix('masyarakat')->group(function () {
         Route::get('/dashboard', [AuthController::class, 'showMasyarakatDashboard'])->name('dashboard');
+        Route::get('/profil', [ProfileController::class, 'edit'])->name('masyarakat.profil.edit');
+        Route::put('/profil', [ProfileController::class, 'update'])->name('masyarakat.profil.update');
         Route::get('/lapor', [LaporanController::class, 'create'])->name('laporan.create');
         Route::post('/lapor', [LaporanController::class, 'store'])->name('laporan.store');
         Route::get('/riwayat', [LaporanController::class, 'index'])->name('laporan.history');
         Route::get('/lapor/{id}', [LaporanController::class, 'show'])->name('laporan.show');
+        Route::get('/statistik', [StatistikController::class, 'index'])->name('masyarakat.statistik');
+        
     });
 
     Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
